432bead2d3cddf77b1c1a5c477e83115621b2d69
nislo7 <st.mockus@gmail.com>
2026-02-20 16:16:25 +0100

    added prisma changes to commit

---
+++ backend/dist/generated/prisma/enums.d.ts
@@ -0,0 +1,9 @@
+export declare const OrderStatus: {
+    readonly PENDING: "PENDING";
+    readonly CONFIRMED: "CONFIRMED";
+    readonly IN_TRANSIT: "IN_TRANSIT";
+    readonly DELIVERED: "DELIVERED";
+    readonly CANCELLED: "CANCELLED";
+};
+export type OrderStatus = (typeof OrderStatus)[keyof typeof OrderStatus];
+//# sourceMappingURL=enums.d.ts.map
