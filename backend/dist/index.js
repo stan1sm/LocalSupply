523ff0444945b6dc00c1c388353a9b57fe2104d9
nislo7 <st.mockus@gmail.com>
2026-02-24 15:37:24 +0100

    deployed on vercel and fixed CORS

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,6 +1,5 @@
-import dotenv from 'dotenv';
+import 'dotenv/config';
 import app from './app.js';
-dotenv.config();
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,5 @@
-import dotenv from 'dotenv'
+import 'dotenv/config'
 import app from './app.js'
-
-dotenv.config()
 
 const port = process.env.PORT ? Number(process.env.PORT) : 3000
 
